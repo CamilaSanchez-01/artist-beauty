bd4563d324fc79e421bbb2f6f4bfad49546a0c61
Camila Sanchez Parra <sanchez.camila@uabc.edu.mx>
2026-05-17 17:29:02 -0700

    Add superuser creation to build script
    Add script to create a superuser if it doesn't exist

--- build.sh
+++ build.sh
@@ -12,3 +12,14 @@ python manage.py migrate
 
 # Carga datos de ejemplo solo si la base está vacía
 python manage.py seed_data || true
+
+# Crear Administradores
+python manage.py shell -c "
+from django.contrib.auth import get_user_model;
+User = get_user_model();
+username='Camilaadmin';
+password='Cami123!';
+email='sanchez.camila@uabc.edu.mx';
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+"
